de13724826fca4c659b70fad9c53f49f94821bfa
jipeng <jipeng@able-elec.com>
2020-02-07 12:07:35 +0800

    异常é处理

---
+++ src/main/java/com/itheima/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,15 @@
+package com.itheima.advice;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionAdvice {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public String deniedExceptionAdvice(AccessDeniedException e){
+        //return "forward:/403.jps";
+        return "redirect:/403.jsp";
+    }
+}
