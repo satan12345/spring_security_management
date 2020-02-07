b107e493cbf81dfbd2331ea82530adaf12d84c93
jipeng <jipeng@able-elec.com>
2020-02-07 11:27:40 +0800

    权限注解添加

--- src/main/java/com/itheima/controller/ProductController.java
+++ src/main/java/com/itheima/controller/ProductController.java
@@ -1,11 +1,19 @@
 package com.itheima.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+    /**
+     *
+     * @return
+     */
+    //@Secured("ROLE_PRODUCT")//security内部使用的注解
+    //@RolesAllowed("ROLE_PRODUCT")//jsr250使用的注解
+    @PreAuthorize("hasAnyRole('ROLE_PRODUCT')")//spring的el表达式
     @RequestMapping("/findAll")
     public String findAll(){
         return "product-list";
